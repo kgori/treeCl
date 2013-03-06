a0fa4565189d572f097bb2369f73d2ddfe1b37b3
Kevin Gori <kgori@ebi.ac.uk>
2013-03-06 00:28:55 +0000

    Adding missing externals/*.py files

---
+++ treeCl/externals/__init__.py
@@ -0,0 +1,5 @@
+from external_software import ExternalSoftware
+from gtp import GTP
+from tree_builders import Phyml, TreeCollection, runPhyml, runTC
+from dv_wrapper import DVWrapper, runDV
+from alf import ALF, simulate_from_tree
