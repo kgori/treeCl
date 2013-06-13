2bb743f7ea78838675a1bc1e3c8dcfc4f37fe37a
Malcolm Perry <mgp32@cam.ac.uk>
2013-06-13 17:03:53 +0100

    scripts

--- scripts/usage.py
+++ scripts/usage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 sys.path.append('/home/malcolm/Documents/EBi/Git/')
 from treeCl.collection import Collection, Scorer
@@ -8,15 +9,17 @@ from treeCl.algorithms import emtrees
 
 c = Collection(input_dir='/home/malcolm/Documents/EBi/Data/easy_case', compression='gz', file_format='phylip', datatype='protein')
 c.calc_NJ_trees() #add verbosity=1 or higher to get progress messages
-dm = c.distance_matrix('euc')
-cl = Clustering(dm)
-p = cl.hierarchical(4, 'single') # should give fairly inaccurate clustering
-true = Partition(tuple([1]*15+[2]*15+[3]*15+[4]*15))
-sc = Scorer(c.records,c.analysis)
+#dm = c.distance_matrix('euc')
+#cl = Clustering(dm)
+#p = cl.hierarchical(4, 'single') # should give fairly inaccurate clustering
+#true = Partition(tuple([1]*15+[2]*15+[3]*15+[4]*15))
+#sc = Scorer(c.records,c.analysis)
 
-sc.score(true)
-sc.score(p)
+#sc.score(true)
+#sc.score(p)
+
+# emtrees usage:
 
 e = emtrees(c,4)
-# e.maximise('ml')
-# print e.L
+e.maximise_test('dist')
+print e.L
