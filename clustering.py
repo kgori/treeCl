457920c09fcdab73379b9085cc59cb678f0c6299
kgori <kgori@ebi.ac.uk>
2013-07-08 21:29:03 +0100

    Can set tolerance on spectral_rotate

--- clustering.py
+++ clustering.py
@@ -193,8 +193,6 @@ class Clustering(object):
         est.fit(coords)
         return Partition(est.labels_)
 
-    # NOT FIXED YET (26/02/13)
-
     def plot_dendrogram(self, compound_key):
         """ Extracts data from clustering to plot dendrogram """
 
@@ -218,8 +216,6 @@ class Clustering(object):
         plt.ylabel('Distance')
         return fig
 
-    # NOT FIXED YET (26/02/13)
-
     def spectral_rotate(
         self,
         decomp=None,
@@ -227,6 +223,7 @@ class Clustering(object):
         max_groups=None,
         min_groups=2,
         verbose=True,
+        tolerance=0.0025,
         **kwargs
         ):
 
@@ -242,7 +239,7 @@ class Clustering(object):
                              / 3))
         (nclusters, clustering, quality_scores, rotated_vectors) = \
             self.cluster_rotate(decomp.vecs, max_groups=max_groups,
-                                min_groups=min_groups)
+                                min_groups=min_groups, tolerance=tolerance)
 
         translate_clustering = [None] * len(M)
         no_of_empty_clusters = 0
@@ -272,13 +269,12 @@ class Clustering(object):
 
         # ######################
 
-    # NOT FIXED YET (26.02.13)
-
     def cluster_rotate(
         self,
         eigenvectors,
         max_groups,
         min_groups=2,
+        tolerance=0.0025,
         ):
 
         groups = range(min_groups, max_groups + 1)
@@ -306,7 +302,7 @@ class Clustering(object):
         index = quality_scores.index(max_score)
         start = index + 1
         for (i, score) in enumerate(quality_scores[index + 1:], start=start):
-            if abs(score - max_score) < 0.0025:
+            if abs(score - max_score) < tolerance:
                 index = i
 
         return (groups[index], clusters[index], quality_scores,
