3a70f7e60205ada0f683450af43b35ace6c99abe
Malcolm Perry <mgp32@cam.ac.uk>
2013-07-17 12:42:53 +0100

    added optimiser to top-level imports

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from collection import Collection, Scorer
 from clustering import Clustering, Partition
 from distance_matrix import DistanceMatrix
+from optimiser import Optimiser
 from plotter import Plotter
 from lib.remote.utils import fileIO
