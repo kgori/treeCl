85c5afd5c46b485e1f099d3e3a4c54ffe04b606e
Malcolm Perry <mgp32@cam.ac.uk>
2013-07-20 17:18:55 +0100

    process multiple folders

--- scripts/results_script.py
+++ scripts/results_script.py
@@ -4,25 +4,32 @@ import analysis
 import matplotlib.pyplot as plt
 from numpy import mean
 import sys
+import os
 
 try:
-    folder = sys.argv[1]
+    folders = sys.argv[1:]
 except:
     folder = './'
 
-results = analysis.foldersearch(folder)
 
-plt.hold(False)
+def plot(folder):
+    results = analysis.foldersearch(folder)
 
-for r in results:
-    plotname = 'plot_' + str(r.id)
-    r.plot(plotname, 'png')
+    plt.hold(False)
 
-lls = [r.likelihood for r in results]
-times = [r.cputime for r in results]
+    for r in results:
+        plotname = os.path.join(folder, 'plot_' + str(r.id))
+        r.plot(plotname, 'png')
 
-print 'Mean Likelihood:' + str(mean(lls))
-print 'Mean time:' + str(mean(times))
+    lls = [r.likelihood for r in results]
+    times = [r.cputime for r in results]
 
-plt.scatter(times, lls)
-plt.savefig('scatterplot')
+    print 'For folder {}:'.format(folder)
+    print 'Mean Likelihood:' + str(mean(lls))
+    print 'Mean time:' + str(mean(times))
+
+    plt.scatter(times, lls)
+    plt.savefig(os.path.join(folder, 'scatterplot'))
+
+for folder in folders:
+    plot(folder)
