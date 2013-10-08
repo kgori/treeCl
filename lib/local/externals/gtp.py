e98a2ba8a9a9186cf4e70c3ecfebabf607917f21
kgori <kgori@ebi.ac.uk>
2013-10-08 10:54:43 +0100

    Changes to distance_matrix and GTP.py to decrease memory requirements: distance matrix is now filled in diagonal blocks - each is a separate call to GTP.jar. Off-diagonals are filled singly in separate calls to GTP.jar. This is massively time consuming, but memory requirements are much less.
    Bump remote version.

--- lib/local/externals/gtp.py
+++ lib/local/externals/gtp.py
@@ -97,3 +97,74 @@ class GTP(ExternalSoftware):
             tmpf.write('\n'.join(self.get_safe_newick_string(tree) for tree in
                        trees))
         self.add_tempfile('{0}/geotrees.nwk'.format(self.tmpdir))
+
+
+def breakpoints(n, m=100):
+    """
+    Takes an integer, returns k approx. equally sized integers,
+    all < m, that sum to n
+    """
+    if n <= m:
+        return n
+
+    k = n / m + (1 if n % m > 0 else 0)
+    s = n / k
+    l = [s for _ in range(k)]
+
+    r = n - sum(l)
+    for i in range(r):
+        l[i] += 1
+
+    assert(sum(l) == n) # debug
+    return np.array([0]+l).cumsum()
+
+def offdiagonal_indices(breakpoints):
+    matrix_length = breakpoints[-1]
+    rows = []
+    cols = []
+    for i, curr in enumerate(breakpoints[1:], start=1):
+        prev = breakpoints[i-1]
+        for j in range(prev, curr):
+            rows.extend([j]*(matrix_length-curr))
+            cols.extend(range(curr,matrix_length))
+    return rows, cols
+
+def fill_diagonal_blocks(matrix, submatrices):
+    i = 0
+    j = 0
+    for subm in submatrices:
+        if subm.shape == (1,):
+            r = c = 1
+        else:
+            r, c = subm.shape
+        matrix[i:i+r, j:j+c] = subm
+        i += r
+        j += c
+    return matrix
+
+def geodist_diagonals(matrix, trees, breakpoints, calculator):
+    """
+    Calculator is GTP object
+    """
+    i = 0
+    submatrices = []
+    for b in breakpoints[1:]:
+        tree_sublist = trees[i:b]
+        i = b
+        submatrices.append(calculator.run(tree_sublist))
+    fill_diagonal_blocks(matrix, submatrices)
+
+def geodist_offdiagonals(matrix, trees, breakpoints, calculator):
+    for (r,c) in zip(*offdiagonal_indices(breakpoints)):
+        matrix[r,c] = matrix[c,r] = calculator.pairwise(trees[r], trees[c])
+
+def geodist(trees, tmpdir, breaks=None):
+    g = GTP(tmpdir=tmpdir)
+    if breaks is None:
+        return g.run(trees)
+    l = len(trees)
+    bps = breakpoints(l, breaks)
+    matrix = np.zeros((l,l))
+    geodist_diagonals(matrix, trees, bps, g)
+    geodist_offdiagonals(matrix, trees, bps, g)
+    return matrix
