8365067183feb7e93857cef77a900ace1927e7d4
Kevin Gori <kgori@ebi.ac.uk>
2014-11-25 16:01:32 +0000

    Fixed typos on l307, l343

--- treeCl/collection.py
+++ treeCl/collection.py
@@ -303,8 +303,8 @@ class Collection(object):
             if delete:
                 to_delete.append(filename)
             # Get input dict
-            data = {'alpha': rec.parameters.partitions.alpha, 'frequencies': rec.parameters.partitions.frequency}
-            if rec.is_dna(i):
+            data = {'alpha': rec.parameters.partitions.alpha, 'frequencies': rec.parameters.partitions.frequencies}
+            if rec.is_dna():
                 data['rates'] = rec.parameters.partitions.rates
             model['partitions'][0] = data
             # Launch local task
