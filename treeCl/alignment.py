e05908d12bc3bcbf2a794c8f8845c1e53233bea6
Kevin Gori <kgori@ebi.ac.uk>
2015-11-09 23:13:27 +0000

    Replace bpp functions

--- treeCl/alignment.py
+++ treeCl/alignment.py
@@ -1,19 +1,26 @@
 import collections
+import itertools
 import os
 import tempfile
 
-from numpy import log
+import numpy as np
 
 import bpp
 from parameters import Parameters, PartitionParameters
-from utils import fileIO
+from utils import fileIO, alignment_to_partials
+from distance_matrix import DistanceMatrix
 from Bio.Seq import Seq, UnknownSeq
 from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
 from Bio.Alphabet import IUPAC
 from Bio import AlignIO
 from collections import defaultdict
-
+from phylo_utils.likcalc import discrete_gamma, sitewise_lik, sitewise_lik_derivs
+from phylo_utils import seq_to_partials
+from phylo_utils.markov import TransitionMatrix
+from phylo_utils.models import LG, GTR
+import logging
+logger = logging.getLogger(__name__)
 
 class Alignment(bpp.Alignment):
     def __init__(self, *args):
@@ -95,8 +102,8 @@ class Alignment(bpp.Alignment):
 
         for v in weights.itervalues():
             if v > 1:
-                ucl += v * log(v)
-        return ucl - n * log(n)
+                ucl += v * np.log(v)
+        return ucl - n * np.log(n)
 
     def to_biopython_msa(self):
         alph = IUPAC.extended_dna if self.is_dna() else IUPAC.extended_protein
@@ -104,3 +111,89 @@ class Alignment(bpp.Alignment):
         for seq in msa: seq.description=''
         return msa
 
+def pairdists(alignment, ncat=4, tolerance=1e-6):
+    """ Load an alignment, calculate all pairwise distances and variances """
+    def calc(brlen):
+        """
+        Inner function calculates l'hood + derivs at branch length = brlen
+        """
+        result = sum([sitewise_lik_derivs(tm.get_p_matrix(gamma_rates[k]*brlen),
+                                          tm.get_dp_matrix(gamma_rates[k]*brlen),
+                                          tm.get_d2p_matrix(gamma_rates[k]*brlen),
+                                          tm.freqs, partials[key1], partials[key2])*(1.0/ncat)
+                              for k in range(ncat)])
+        lk = np.log(result[:,0]).sum()
+        dlk = (result[:,1]/result[:,0]).sum()
+        d2lk = ((result[:,0]*result[:,2] - result[:,1]**2)/result[:,0]**2).sum()
+        return lk, dlk, d2lk
+
+    def get_step(dlk, d2lk):
+        step = dlk / np.abs(d2lk) # abs makes optimiser backtrack from a minimum likelihood
+        while (brlen + step) < 0:
+            step *= 0.5
+        return step
+
+    try:
+        model = alignment.parameters.partitions.model
+        freqs = alignment.parameters.partitions.frequencies
+        alpha = alignment.parameters.partitions.alpha
+    except:
+        logger.error('No parameters available')
+        return
+
+    if model == 'LG':
+        subs_model = LG(freqs)
+    elif model == 'GTR':
+        rates = alignment.parameters.partitions.rates
+        subs_model = GTR(rates, freqs, True)
+    else:
+        raise ValueError("Can't handle this model: {}".format(model))
+
+    # Set up markov model
+    tm = TransitionMatrix(subs_model)
+    gamma_rates = discrete_gamma(alpha, ncat)
+    partials = alignment_to_partials(alignment)
+    seqnames = alignment.get_names()
+    nseq = len(seqnames)
+    distances = np.zeros((nseq, nseq))
+    variances = np.zeros((nseq, nseq))
+
+    for i, j in itertools.combinations(range(nseq), 2):
+        key1 = seqnames[i]
+        key2 = seqnames[j]
+        maxiter = 100
+
+        brlen = 1.0  # just have a guess
+        lk, dlk, d2lk = calc(brlen)
+        maxlk = lk
+        niter = 0
+        step = get_step(dlk, d2lk)
+
+        # This is the newton optimiser
+        while True:
+            niter += 1
+            if niter > maxiter:
+                break  # failed to converge somehow
+
+            # Do the calculation to work out the new step
+            lk, dlk, d2lk = calc(brlen + step)
+            if (lk - maxlk) < -1000*tolerance:
+                # the likelihood got worse, so the step was too big
+                # so restore the old values and halve the step, try again
+                step *= 0.5
+                continue
+
+            else:
+                # successful move. update brlen
+                brlen = brlen + step
+                maxlk = lk
+                step = get_step(dlk, d2lk)
+
+            if np.abs(dlk) < tolerance:
+                break  # Converged
+
+        distances[i, j] = distances[j, i] = brlen
+        variances[i, j] = variances[j, i] = np.abs(-1/d2lk)
+    dm = DistanceMatrix.from_array(distances, names=seqnames)
+    vm = DistanceMatrix.from_array(variances, names=seqnames)
+    return dm, vm
